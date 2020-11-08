8684f69392b4802b875a4cd1b8569c8d3c716ed4
unknown <name>
2020-11-08 16:16:10 +0600

    Add some vue  route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,40 @@
 require('./bootstrap');
+// Vue
+window.Vue = require('vue')
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+// Vue Form
+import {Form, HasError, AlertError} from 'vform'
+window.form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+// Componets
+import Dashboard from './components/Dashboard'
+import Profile from './components/Profile'
+import Users from './components/Users'
+
+let routes = [{
+    path: '/dashboard',
+    component: Dashboard
+}, {
+    path: '/profile',
+    component: Profile
+}, {
+    path: '/users',
+    component: Users
+}]
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+// Vue.component('exmaple-component', require('./components/ExampleComponents.vue'));
+
+const app = new Vue({
+    el: '#app',
+    router
+});
+
+export default app
---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,12 @@
+<template v-if="ok">
+  <h1>Dashboard</h1>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log('ok');
+        
+    }
+}
+</script>
